8355ad26cc8ff74bc12169723f6391f2c261da78
EdenrocIT <it.support@edenroccapcana.com>
2026-09-24 17:18:53 -0400

    Primer Commit

---
+++ src/server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const app = express();
+const PORT = process.env.PORT || 3010;
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.json({ message: 'API funcionando' });
+});
+
+app.listen(PORT, () => {
+  console.log(`Servidor escuchando en http://localhost:${PORT}`);
+});
